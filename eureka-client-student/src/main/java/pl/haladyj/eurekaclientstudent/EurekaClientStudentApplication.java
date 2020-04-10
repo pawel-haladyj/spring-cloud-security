ebf20088a8e7a690d4484fae0cef0b53602c3bbe
Pawel Haladyj <pawel@haladyj.pl>
2020-04-10 13:09:11 +0200

    enabled eureka server

---
+++ eureka-client-student/src/main/java/pl/haladyj/eurekaclientstudent/EurekaClientStudentApplication.java
@@ -0,0 +1,13 @@
+package pl.haladyj.eurekaclientstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientStudentApplication.class, args);
+	}
+
+}
---
+++ eureka-client-student/src/test/java/pl/haladyj/eurekaclientstudent/EurekaClientStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.eurekaclientstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
